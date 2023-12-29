612d81b47daa2836ec862d8f864ccbfb1a7c0601
Yousef Titi <youseftiti08@gmail.com>
2023-12-30 00:19:20 +0200

    Update py.py

--- mysql/py.py
+++ mysql/py.py
@@ -9,7 +9,7 @@ def local_css(file_name):#هذا الفنقشن مشان اجيب التصميم
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("style/style.css")
+#local_css("style/style.css")
 
 
 
